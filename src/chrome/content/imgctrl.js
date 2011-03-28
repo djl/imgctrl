fca6dcc452dde8d72f5f0516c2ba30393b068164
David Logie <d@djl.im>
2011-03-28 18:08:32 +0100

    Namespace *all* the functions.

--- src/chrome/content/imgctrl.js
+++ src/chrome/content/imgctrl.js
@@ -1,11 +1,11 @@
 window.addEventListener("load", function(event) {
     var menu = document.getElementById("contentAreaContextMenu");
     if (menu) {
-        menu.addEventListener("popupshowing", setupMenuItems, false);
+        menu.addEventListener("popupshowing", imgcrtlInit, false);
     }
 }, false);
 
-function setupMenuItems(event) {
+function imgcrtlInit(event) {
     var menuItems = ['window', 'tab'];
     for (var i=0; i < menuItems.length; i++) {
         var item = document.getElementById('imgctrl-' +  menuItems[i]);
